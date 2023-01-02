c3365c1bacf13c73d1157df62b89b4aa282b2a0b
asyncFrank <frank.maldonado.developer@gmail.com>
2023-01-02 16:30:16 -0300

    lst2-previa

--- src/data/fonts.js
+++ src/data/fonts.js
@@ -6,12 +6,12 @@ export const locations = [
   },
   {
     name: "Citron Indústria e Comércio de Produtos Químicos Ltda",
-    position: [-23.4, -46.7],
+    position: [-23.5500, -46.6500],
     icon: "fertilizantes-agk",
   },
   {
     name: "Volcano Agrociencia Industria e Comercio de Defensivos Agricolas Ltda",
-    position: [-23.4, -46.8],
+    position: [-23.5590, -46.6285],
     icon: "fertilizantes-agk",
   },
   { name: "Fosforita Olinda", position: [-7.83437, -34.9064],icon:'stone' },
